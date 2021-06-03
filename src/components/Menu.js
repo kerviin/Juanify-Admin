8f834f7f5621f88cf6cad09eb787002ca2e45c46
kerviin <kervinterencereyes17@gmail.com>
2021-06-03 18:29:38 +0800

    changed a few words, added juanify icon

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import logo from '../images/juanifynew.png'
 
 export default function Menu() {
     return (
         <aside className="main-sidebar sidebar-dark-primary elevation-4">
   {/* Brand Logo */}
   <a href="index3.html" className="brand-link">
-    <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{opacity: '.8'}} />
-    <span className="brand-text font-weight-light">AdminLTE 3</span>
+    <img src= {logo} alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{opacity: '.8'}} />
+    <span className="brand-text font-weight-light">Juanify</span>
   </a>
   {/* Sidebar */}
   <div className="sidebar">
